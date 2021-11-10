67991729c769d298a131b2947871f94f9f05a856
maififteen <maififteen.chan@gmail.com>
2021-11-10 21:51:33 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from tensorflow import keras
+
+app = Flask(__name__)
+app.debug = True
+
+model = keras.models.load_model('model.h5')
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    prediction = model.predict(features)
+    return render_template('index.html', output=prediction)
+
+if __name__ == '__main__':
+    app.run(use_reloader=True)
