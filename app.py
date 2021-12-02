17b339023a679a31060593dcba46fbd1bb284f39
maififteen <maififteen.chan@gmail.com>
2021-12-02 21:14:08 +0700

    Refactor all pages

--- app.py
+++ app.py
@@ -3,26 +3,29 @@ from tensorflow import keras
 
 app = Flask(__name__)
 app.debug = True
-
+# TODO : Add prediction feature
+# TODO : Add dashboard
 model = keras.models.load_model('model.h5')
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
     return render_template('index.html')
 
 @app.route('/login')
-def login_page():
-    return render_template('login.html')
+def login():
+    return render_template('auth/login.html')
 
 @app.route('/register')
-def register_page():
-    return render_template('register.html')
+def register():
+    return render_template('auth/register.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/predict')
 def predict():
-    features = [float(x) for x in request.form.values()]
-    prediction = model.predict(features)
-    return render_template('index.html', output=prediction)
+    return render_template('predict.html')
 
 if __name__ == '__main__':
     app.run(use_reloader=True)
